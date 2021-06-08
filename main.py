335d3b1fd7ab3ad3c3f1cd05c3166ac0a2a4aed3
tremor6916 <lpawaon@gmail.com>
2021-06-08 21:50:58 +0800

    pygame screen initialization

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import pygame
 import random
 import time
 
-width, height = 1920, 1080
+width, height = 1280, 780
+bg_color = (255,255,255)
+running = True
+
 pygame.init()
 pygame.mixer.init()
 pygame.display.set_caption("Pathfinder Visualizer")
 window = pygame.display.set_mode((width, height))
+window.fill(bg_color)
 
-pygame.display.update()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
