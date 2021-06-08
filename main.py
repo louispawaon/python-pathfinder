b6049c2694a01803c865319ba0453e055319eee0
tremor6916 <lpawaon@gmail.com>
2021-06-08 22:00:03 +0800

    pygame screen adjustments

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import time
 width, height = 1280, 780
 bg_color = (255,255,255)
 running = True
+icon = pygame.image.load('maze.png')
 
 pygame.init()
 pygame.mixer.init()
 pygame.display.set_caption("Pathfinder Visualizer")
 window = pygame.display.set_mode((width, height))
 window.fill(bg_color)
+pygame.display.set_icon(icon)
 
 while running:
     for event in pygame.event.get():
