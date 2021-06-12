668eec67bad4bb5830d342d61016b15eca1794a1
tremor6916 <lpawaon@gmail.com>
2021-06-12 20:49:31 +0800

    solved issue regarding maze generation

--- main.py
+++ main.py
@@ -36,119 +36,123 @@ closed=[]
 solution_path={}
 
 #Grid 
-def build_grid(x,y,cell_width):
-    for j in range (20):
-        x=20
-        y=y+20
-        for h in range (20):
-            pygame.draw.line(window, color_BLACK, [x + cell_width, y], [x + cell_width, y + cell_width]) #Right Side
-            pygame.draw.line(window, color_BLACK, [x, y + cell_width],[x , y]) #Left Side
-            pygame.draw.line(window, color_BLACK, [x, y], [x + cell_width, y]) #Top Side
-            pygame.draw.line(window, color_BLACK, [x + cell_width, y + cell_width],[x, y + cell_width]) #Bottom Side
+def build_grid(x, y, cell_width):
+    for i in range(20):
+        x = 20                                                            
+        y = y + 20                                                        
+        for j in range(20):
+            pygame.draw.line(window, color_BLACK, [x, y], [x + cell_width, y]) #Top Wall          
+            pygame.draw.line(window, color_BLACK, [x + cell_width, y], [x + cell_width, y + cell_width]) #Right Wall
+            pygame.draw.line(window, color_BLACK, [x + cell_width, y + cell_width], [x, y + cell_width]) #Bottom Wall
+            pygame.draw.line(window, color_BLACK, [x, y + cell_width], [x, y]) #Left Wall
+            grid.append((x,y))                                            
+            x = x + 20                                                    
+            pygame.display.update
 
-            grid.append((x,y))
-            x=x+20
-            pygame.display.update()
+def top_wall(x, y):
+    pygame.draw.rect(window, color_GREEN, (x + 1, y - cell_width + 1, 19, 39), 0)         
+    pygame.display.update()                                              
 
-#Movement Functions
-def right_wall(x,y):
-    pygame.draw.rect(window, color_GREEN,(y+1,y+1,39,19),0)
+
+def bottom_wall(x, y):
+    pygame.draw.rect(window, color_GREEN, (x +  1, y + 1, 19, 39), 0)
     pygame.display.update()
 
-def left_wall(x,y):
-    pygame.draw.rect(window, color_GREEN,(x-cell_width+1,y+1,39,19),0)
+
+def left_wall(x, y):
+    pygame.draw.rect(window, color_GREEN, (x - cell_width +1, y +1, 39, 19), 0)
     pygame.display.update()
 
-def top_wall(x,y):
-    pygame.draw.rect(window, color_GREEN,(x+1,y-cell_width+1,19,39),0)
+
+def right_wall(x, y):
+    pygame.draw.rect(window, color_GREEN, (x +1, y +1, 39, 19), 0)
     pygame.display.update()
 
-def bottom_wall(x,y):
-    pygame.draw.rect(window, color_GREEN,(x+1,y+1,39,19),0)
+
+def moving_cell( x, y):
+    pygame.draw.rect(window, color_PURPLE, (x +1, y +1, 18, 18), 0)          
     pygame.display.update()
 
-def move_cell(x,y):
-    pygame.draw.rect(window, color_PURPLE,(x+1,y+1,18,18),0)
-    pygame.display.update()
 
-def bactracker_cell(x,y):
-    pygame.draw.rect(window, color_GREEN,(x+1,y+1,18,18),0)
-    pygame.display.update()
+def backtracker_cell(x, y):
+    pygame.draw.rect(window, color_GREEN, (x +1, y +1, 18, 18), 0)        
+    pygame.display.update()                                        
+
 
 def path_tracker(x,y):
-    pygame.draw.rect(window, color_RED,(x+8,y+8,5,5),0)
-    pygame.display.update()
+    pygame.draw.rect(window, color_RED, (x+8, y+8, 5, 5), 0)            
+    pygame.display.update()                                        
 
-#Maze Algorithm Implementation
+
 def maze_logic(x,y):
-    move_cell(x,y)
-    stack.append((x,y))
-    closed.append((x,y))
-
-    while len(stack)>0:
-        time.sleep(0.07)
-        cell=[]
-
-        if(x+cell_width,y) not in closed and (x+cell_width,y)in grid:
-            cell.append("Right")
-
-        if(x-cell_width,y) not in closed and (x-cell_width,y)in grid:
+    moving_cell(x, y)                                              
+    stack.append((x,y))                                            
+    closed.append((x,y))                                          
+    while len(stack) > 0:                                          
+        time.sleep(.07)                                            
+        cell = []                                                 
+        if (x + cell_width, y) not in closed and (x + cell_width, y) in grid:      
+            cell.append("Right")                                   
+        if (x - cell_width, y) not in closed and (x - cell_width, y) in grid:       
             cell.append("Left")
 
-        if(x,y+cell_width) not in closed and (x,y+cell_width)in grid:
+        if (x , y + cell_width) not in closed and (x , y + cell_width) in grid:     
             cell.append("Bottom")
-        
-        if(x,y-cell_width) not in closed and (x,y-cell_width)in grid:
+
+        if (x, y - cell_width) not in closed and (x , y - cell_width) in grid:      
             cell.append("Top")
 
-        if len(cell)>0:
-            current_cell = (random.choice(cell))
+        if len(cell) > 0:                                         
+            cell_chosen = (random.choice(cell))                    
 
-            if (current_cell=="Right"):
-                right_wall(x,y)
-                solution_path[(x+cell_width,y)]=x,y
-                x=x+cell_width
-                closed.append((x,y))
-                stack.append((x,y))
+            if cell_chosen == "Right":                             
+                right_wall(x,y)                                  
+                solution_path[(x + cell_width, y)] = x,y                        
+                x = x + cell_width                                          
+                closed.append((x,y))                              
+                stack.append((x,y))                                
 
-            elif (current_cell=="Left"):
-                left_wall(x,y)
-                solution_path[(x-cell_width,y)]=x,y
-                x=x-cell_width
-                closed.append((x,y))
-                stack.append((x,y))
+            elif cell_chosen == "Left":
+                left_wall(x, y)
+                solution_path[(x - cell_width, y)] = x, y
+                x = x - cell_width
+                closed.append((x, y))
+                stack.append((x, y))
 
-            elif (current_cell=="Top"):
-                top_wall(x,y)
-                solution_path[(x,y-cell_width)]=x,y
-                y=y-cell_width
-                closed.append((x,y))
-                stack.append((x,y))
+            elif cell_chosen == "Top":
+                top_wall(x, y)
+                solution_path[(x , y - cell_width)] = x, y
+                y = y - cell_width
+                closed.append((x, y))
+                stack.append((x, y))
 
-            elif (current_cell=="Bottom"):
-                bottom_wall(x,y)
-                solution_path[(x,y+cell_width)]=x,y
-                y=y+cell_width
-                closed.append((x,y))
-                stack.append((x,y))
+            elif cell_chosen == "Bottom":
+                bottom_wall(x, y)
+                solution_path[(x , y + cell_width)] = x, y
+                y = y + cell_width
+                closed.append((x, y))
+                stack.append((x, y))
 
         else:
-            x,y=stack.pop()
-            move_cell(x,y)
-            time.sleep(0.05)
-            bactracker_cell(x,y)
+            x, y = stack.pop()                                   
+            moving_cell(x, y)                                     
+            time.sleep(.05)                                     
+            backtracker_cell(x, y)                             
+
 
 def tracer(x,y):
-    path_tracker(x,y)
-    while(x,y)!=(20,20):
-        x,y=solution_path[x,y]
-        path_tracker(x, y)
-        time.sleep(0.1)
+    path_tracker(x, y)                                          
+    while (x, y) != (20,20):                                     
+        x, y = solution_path[x, y]                                  
+        path_tracker(x, y)                                      
+        time.sleep(.1)
 
-x, y = 20, 20
-build_grid(40,0,20)
-maze_logic(x,y)
-tracer(400,400)
+
+x, y = 20, 20                     
+build_grid(40, 0, 20)             
+maze_logic(x,y)              
+tracer(400, 400)      
+
 
 #Pygame Loop Logic
 while running:
@@ -157,4 +161,4 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-#BFA, DFS, A* to implement 
+#BFA, DFS, A* to implement in future update
